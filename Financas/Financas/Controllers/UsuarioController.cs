03d53f5166252c274de3458e9f36613824c3f6e4
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-03-24 00:10:57 -0400

    Criando os controllers para cadastro de usuários e utilizando a injeção de dependencias do ninject

---
+++ Financas/Financas/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using Financas.DAO;
+using Financas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public UsuarioController(UsuarioDAO dao)
+        {
+            this.Dao = dao;
+        }
+
+        public UsuarioDAO Dao { get; private set; }
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(Dao.ListaTodos());
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Usuario Usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                Dao.Adiciona(Usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", Usuario);
+            }
+        }
+
+    }
+}
---
+++ Financas/Financas/DAO/UsuarioDAO.cs
@@ -0,0 +1,29 @@
+﻿using Financas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class UsuarioDAO
+    {
+        public UsuarioDAO(FinancasContext context)
+        {
+            this.Context = context;
+        }
+
+        public FinancasContext Context { get; private set; }
+
+        public void Adiciona (Usuario Usuario)
+        {
+            Context.UsuariosContext.Add(Usuario);
+            Context.SaveChanges();
+        }
+
+        public IList<Usuario> ListaTodos()
+        {
+            return Context.UsuariosContext.ToList();
+        }
+    }
+}
