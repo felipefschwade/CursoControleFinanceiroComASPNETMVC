19d6ea3b711869b562934a4f0a2bab2aaa0a8ea5
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-03-23 23:43:03 -0400

    Criando views e as entidades

---
+++ Financas/Financas/DAO/FinancasContext.cs
@@ -0,0 +1,20 @@
+﻿using Financas.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Movimentacao> MovimentacoesContext { get; set; }
+        public DbSet<Usuario> UsuariosContext { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Financas/Financas/Models/Movimentacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.Models
+{
+    public class Movimentacao
+    {
+        public virtual Usuario Usuario { get; set; }
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public int UsuarioId { get; set; }
+        public Tipo Tipo { get; set; }
+    }
+}
